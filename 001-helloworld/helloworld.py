4a19801d5ce0e5c003686bd1ff4be12ab3c16c2a
luxwarp <mikael.m.carlsson@gmail.com>
2019-09-10 09:36:47 +0200

    added variables

--- 001-helloworld/helloworld.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-# prints "Hello world" to console.
-print("Hello world")
---
+++ 003-variables/examples/variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# create a variable and assign value.
+
+name = "Luxwarp"
+age = 29
+
+print("My name:", name)
+print("Age:", age)
+
+print("Name is type:", type(name))
+print("Age is type:", type(age))
+
+# lets add 1 to age
+age += 1
+print("Age is now:", age)
