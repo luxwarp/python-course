58c4e41b2689e4a841d320430f81280b86d964d9
luxwarp <mikael.m.carlsson@gmail.com>
2019-09-10 12:00:54 +0200

    added booleans

---
+++ 003-datatypes/examples/booleans.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# booleans are values like True, False
+# you can use booleans to check for if a certain statement is true or false.
+# IMPORTANT: booleans is typed True, False not true, false
+# is_admin = true # this will generate error.
+
+is_admin = True
+
+print("is_admin type:", type(is_admin))
+print("is_admin value:", is_admin)
+
+has_dog = False
+
+print("has_dog type:", type(has_dog))
+print("has_dog value:", has_dog)
+
+# a None value can be used to declare a variable without assigning it a value.
+name = None
+print("Name type:", type(name))
+print("Name value:", name)
